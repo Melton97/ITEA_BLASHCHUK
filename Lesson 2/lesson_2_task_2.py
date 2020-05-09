3a33b1e92f305bd23644822c7948db2841d4a078
Melton <melton97zd@gmail.com>
2020-05-09 23:14:16 +0300

    task2

--- Lesson 2/lesson_2_task_2.py
+++ Lesson 2/lesson_2_task_2.py
@@ -1,1 +1,43 @@
-#
+# Создать класс магазина. Конструктор должен инициализировать зачения:
+# 'Название магазина', 'Количество проданных товаров'. 
+# Реализовать методы обьекта, которые будут увеличивать кол-во проданных товаров, 
+# и реализовать вывод значения переменной класса, которая будет хранить 
+# общее кол-во товаров проданных всеми магазинами.
+
+class Market:
+
+    Total_value = []
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def incr(self):
+        Total_value = []
+        Total_value.append(self.value)
+        print(Total_value)
+
+    def summ_markets(self):
+        summ_of_markets = 0
+        Total_value = []
+        for i in Total_value:
+            summ_of_markets +=i
+        print(summ_of_markets)
+
+
+Allo = Market('Allo', 400)
+Citrus = Market('Citrus', 500)
+
+print(Allo.incr())
+print(Citrus.incr())
+
+print(Allo.summ_markets())
+
+# TOTAL = Market('Total', 0)
+# print(TOTAL.summ_markets())
+
+
+
+
+
+
