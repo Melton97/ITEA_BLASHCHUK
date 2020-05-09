f78ee19b793c0d72974824b76a57c1734af52279
Melton <melton97zd@gmail.com>
2020-05-09 22:19:06 +0300

    task1_lesson2

---
+++ Lesson 2/lesson_2_task_1.py
@@ -0,0 +1,40 @@
+# Создать класс автомобиля. Описать общие атрибуты. 
+# Создать классы легкового автомобиля и грузового.
+# Описать в основном классе базовые атрибуты для автомобилей.
+# Бдует плюсом если в классах наследниках переопределите методы базового класса.
+
+class Car:
+
+    description = 'The main class of cars'
+
+    def __init__(self, wheels, motor, body):
+
+        self.wheels = wheels
+        self.motor = motor
+        self.body = body
+
+    def move(self):
+        print('Lets mooovee !')
+
+    def brake(self):
+        print('Braking now !')
+
+class LittleCar(Car):
+
+    def move(self):
+        print('Lessles move...')
+
+class BigCar(Car):
+
+    def brake(self):
+        print('Lessles braking...')
+
+BMW = Car(4, 4.4, 'f15')
+print(BMW.move())
+print(BMW.brake())
+
+Audi = LittleCar(4, 4.0, 8)
+print(Audi.move())
+
+Kamaz = BigCar(10, 3.0, 10)
+print(Kamaz.brake())
