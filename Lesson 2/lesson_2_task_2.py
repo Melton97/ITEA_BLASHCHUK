d996f5c6f27e28c6f6f4420c63f0bc8c244fc51f
Melton <melton97zd@gmail.com>
2020-05-10 20:44:57 +0300

    2_3

--- Lesson 2/lesson_2_task_2.py
+++ Lesson 2/lesson_2_task_2.py
@@ -6,42 +6,20 @@
 
 class Market:
 
-    Total_value = []
+    Total_value = 0
 
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
-    def incr(self):
-        Total_value = []
-        Total_value.append(self.value)
-        print(Total_value)
+    def plusser(self, value):
+        Market.Total_value += value
 
-    def summ_markets(self):
-        summ_of_markets = 0
-        Total_value = []
-        for i in Total_value:
-            summ_of_markets +=i
-        print(summ_of_markets)
-
-    def __add__(self, other):
-        Total_value = Market(name=self.name, value=self.value + other.value)
-        return print(Total_value)
-
-    def __str__(self):
-        return self.name
-        
 
 Allo = Market('Allo', 400)
 Citrus = Market('Citrus', 500)
 
-print(Allo.incr())
-print(Citrus.incr())
-
-Allo+Citrus
-
-# TOTAL = Market('Total', 0)
-# print(TOTAL.summ_markets())
+print(Allo.plusser())
 
 
 
--- Lesson 2/lesson_2_task_3.py
+++ Lesson 2/lesson_2_task_3.py
@@ -11,11 +11,46 @@ class Dot:
 
     def __init__(self, x, y, z):
 
-        self.x = x
-        self.y = y 
-        self.z = z
+        self._x = x
+        self._y = y 
+        self._z = z
 
-    def change(self):
-        pass
+    def get_x(self):
+        return self._x
+        
+    def get_y(self):
+        return self._y
+        
+    def get_z(self):
+        return self._z
+
+    def set_x(self, new_value):
+        self._x = new_value    
+        
+    def set_y(self, new_value):
+        self._y = new_value   
+        
+    def set_z(self, new_value):
+        self._z = new_value
+
+    def __add__(self, other):
+        return self._x + other._x and self._y + other._y and self._z + other._z
+    
+    def __sub__(self, other):
+        return self._x - other._x and self._y - other._y and self._z - other._z
+
+    def __mul__(self, other):
+        return self._x * other._x and self._y * other._y and self._z * other._z
+
+    def __truediv__(self, other):
+        return self._x / other._x and self._y / other._y and self._z / other._z
+
+
+first_dot = Dot(1, 2, 3)
+second_dot = Dot(1, 2, 3)
+
+
+
+print(first_dot.__mul__(second_dot))
 
     
