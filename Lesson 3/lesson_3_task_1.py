1cbaec5b9ad6354a9d346d9afe6335d69686c9b8
Melton <melton97zd@gmail.com>
2020-05-11 13:51:11 +0300

    3

--- Lesson 3/lesson_3_task_1.py
+++ Lesson 3/lesson_3_task_1.py
@@ -1,13 +1,22 @@
 # Создать декоратор с аргументами.
 # Который будет вызывать функцию, определенное кол-во раз, 
 # будет выводить кол-во времени затраченного на выполнение данной функции и ее название.
+import random
 
 def my_decorator(func):
-    value = 3
-    
 
+    def wrapper():
+        i = 10
+        result = i*func()
+        print (result)
+        return result
+        
 
+    return wrapper
 
+@my_decorator
 def my_func():
 
-    print("How it possible?")
+   return random.randint(1, 100)
+
+my_func()
