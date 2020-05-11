f2451d860fe9da2d37f817afc1863319bb1baa1a
Melton <melton97zd@gmail.com>
2020-05-11 18:12:01 +0300

    3

--- Lesson 3/lesson_3_task_1.py
+++ Lesson 3/lesson_3_task_1.py
@@ -8,7 +8,7 @@ def my_decorator(func):
 
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        for i in range (1, 100):
+        for i in range (1, 1000):
             result = func(*args, **kwargs)
             print (result)
         my_time = time.time()-start_time
@@ -20,6 +20,6 @@ def my_decorator(func):
 
 @my_decorator
 def my_func():
-   return random.randint(1, 20)
+   return random.randint(1, 100)
 
 my_func()
