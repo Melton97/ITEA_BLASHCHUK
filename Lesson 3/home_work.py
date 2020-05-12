6ec6aa6c6fa04e2dd3fb883f553f0242353a6845
Melton <melton97zd@gmail.com>
2020-05-12 22:01:46 +0300

    q

--- Lesson 3/home_work.py
+++ Lesson 3/home_work.py
@@ -18,11 +18,28 @@ class MyStack:
             my_stack.append(a)
         print (my_stack)
 
+class MyRow:
 
+    def __init__(self, rand, val):
+        self.rand = rand
+        self.val = val
+
+    def init_row(self, rand, val):
+        my_row = deque()
+        for i in range(1, self.val):
+            a = random.randint(1, self.rand)
+            my_row.append(a)
+        print (my_row)
+
+class Complex:
+    pass
+
+    #делаем комплексное число и переопределяем для него ариф операции , нужен будет str
 
 q = MyStack(100, 1000)
-
+t = MyRow(10, 10)
 q.init_stack(100, 1000)
+t.init_row(10, 10)
             
 
 
